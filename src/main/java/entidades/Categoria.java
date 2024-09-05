6099b59f0c2b07440d945a407006c9158e64f86a
MatiasRodriguez30 <119769021+MatiasRodriguez30@users.noreply.github.com>
2024-09-05 14:10:13 -0300

    Add files via upload

---
+++ src/main/java/entidades/Categoria.java
@@ -0,0 +1,27 @@
+package entidades;
+import jdk.jfr.Enabled;
+
+import lombok.*;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="categoria")
+@NoArgsConstructor
+
+public class Categoria implements Serializable{
+    private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String denominacion;
+
+    @ManyToMany(mappedBy = "categoria")
+    private List<Articulo> articulo = new ArrayList<Articulo>();
+    public Categoria(String denominacion){
+        this.denominacion=denominacion;
+    }
+}
---
+++ src/main/java/entidades/DetalleFac.java
@@ -0,0 +1,29 @@
+package entidades;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import lombok.*;
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+@Table(name="detalleFac")
+@Data
+@NoArgsConstructor
+public class DetalleFac implements Serializable{
+    private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int cantidad;
+    private int subtotal;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "fk_factura")
+    private Factura factura;
+
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    @JoinColumn(name="fk_articulo")
+    private Articulo articulo;
+
+    public DetalleFac(int cantidad, int subtotal){
+        this.cantidad=cantidad;
+        this.subtotal=subtotal;
+    }
+}
